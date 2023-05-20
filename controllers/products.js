05781d997a2a59eaadad726980c81ff435988ed9
ndibonk@gmail.com <petergachau>
2023-05-20 09:37:58 +0300

    gg

--- controllers/products.js
+++ controllers/products.js
@@ -29,7 +29,7 @@ export const createTour = async (req, res) => {
     await newTour.save();
     res.status(201).json(newTour);
   } catch (error) {
-    res.status(404).json({ message: "Something went wrong" });
+    res.status(404).json({ message: "Something went wrongs" });
   }
 };
 
